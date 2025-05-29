a2ab8547c510b69daefb332dda4017823e10e571
Samirborges <alessandro.samir@souunit.com.br>
2025-05-29 15:26:26 -0300

    Atualizações arvore rubro negro
    -> Adição de regras básicas
    1. Obrigação do nó raiz ser preto
    2. Regra de verificação se o nó pai é vermelho ou preto, caso seja vermelho o nó filho se torna preto;
    3. Método de finalização da árvore onde torna todos os nós folha em preto

--- Arvore_RedBlack.py
+++ Arvore_RedBlack.py
@@ -1,22 +1,42 @@
 from ArvoreAVL import ArvoreAVL
-from Celula import Direction, Celula
+from Celula import Direction, Celula, Color
 
 class Arvore_RedBlack(ArvoreAVL):    
-    def adicionar(self, node_father, node_add):
+    def adicionar(self, node_father: Celula, node_add: Celula):
         super().adicionar(node_father, node_add)
+        self.verificar_cor_no(node_father, node_add)
+        self.RAIZ.color = Color.PRETO
+        # self.transformar_nos_folha()    
+    
+    def verificar_cor_no(self, node_father: Celula, node_add: Celula):
+        if node_add.color == 'Vermelho' and node_father.color == 'Vermelho': node_add.color = Color.PRETO
+    
+    def transformar_nos_folha(self):
+        lista_nos_folhas = self.identify_node_sheet()
+        for no in lista_nos_folhas: no.color = Color.PRETO
+    
+    def verificar_no(self, node):
+        retorno = super().verificar_no(node)
+        retorno['Cor'] = node.color
+        return retorno
+    
+    def finalizar_arvore(self):
+        self.transformar_nos_folha()
         
     
 # Área de testes ----------------------------------------------------------------------------------------------------------------------------
 def executa_avl1():
     avl1 = Arvore_RedBlack('27')
-    avl1.adicionar(avl1.found_index_node('27') , Celula(None, None, '15', Direction.ESQUERDA))
-    avl1.adicionar(avl1.found_index_node('27') , Celula(None, None, '29', Direction.DIREITA))
-    avl1.adicionar(avl1.found_index_node('15') , Celula(None, None, '16', Direction.ESQUERDA))
+    avl1.adicionar(avl1.found_index_node('27') , Celula(None, None, '15', Direction.ESQUERDA, Color.PRETO))
+    avl1.adicionar(avl1.found_index_node('27') , Celula(None, None, '29', Direction.DIREITA, Color.VERMELHO))
+    avl1.adicionar(avl1.found_index_node('15') , Celula(None, None, '16', Direction.ESQUERDA, Color.VERMELHO))
     
     # Desbalanceando a árvore
-    avl1.adicionar(avl1.found_index_node('16'), Celula(None, None, '17', Direction.ESQUERDA))
-    avl1.adicionar('29', Celula(None, None, '13', Direction.DIREITA))
-    avl1.adicionar('13', Celula(None, None, '12', Direction.DIREITA))
+    avl1.adicionar(avl1.found_index_node('16'), Celula(None, None, '17', Direction.ESQUERDA, Color.VERMELHO))
+    avl1.adicionar(avl1.found_index_node('29'), Celula(None, None, '13', Direction.DIREITA, Color.PRETO))
+    avl1.adicionar(avl1.found_index_node('13'), Celula(None, None, '12', Direction.DIREITA, Color.VERMELHO))
+    
+    avl1.finalizar_arvore()
     
     print('AVL 1')
     for node in list(avl1.Tree): print(avl1.verificar_no(node))
